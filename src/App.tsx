a3aa7d0de63ceda4665e868e4ef4df77b4bbec74
Pawel-W88 <rws.pawlak@gmail.com>
2024-03-12 14:17:25 +0100

    add: hero images and images slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import { ImageSlider } from "./Components/ImageSlider/ImageSlider";
+import hero1 from "./assets/hero1-unsplash.jpg";
+import hero2 from "./assets/hero2-unsplash.jpg";
+import hero3 from "./assets/hero3-unsplash.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const sliderImages = [hero1, hero2, hero3];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ImageSlider images={sliderImages} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
