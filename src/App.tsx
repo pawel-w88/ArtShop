43b0a1f8957a299d0b5819a9fc86961a3fe1899d
Pawel-W88 <rws.pawlak@gmail.com>
2024-03-19 11:28:53 +0100

    add: themaContext & reactSwitch & styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,55 @@
+import { useState } from "react";
 import { ImageSlider } from "./Components/ImageSlider/ImageSlider";
 import { Gallery } from "./Components/Gallery/Gallery";
 import { images } from "./data.ts";
 import hero1 from "./assets/hero1-unsplash.jpg";
 import hero2 from "./assets/hero2-unsplash.jpg";
 import hero3 from "./assets/hero3-unsplash.jpg";
-import "./App.scss";
 import { Navbar } from "./Components/Navbar/Navbar.tsx";
 import { Button } from "./Components/Button/Button.tsx";
+import ThemeContext from "./ThemeContext";
+import ReactSwitch from "react-switch";
+import { CloudMoon, Sun } from "lucide-react";
+import "./App.scss";
 
 function App() {
   const sliderImages = [hero1, hero2, hero3];
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <>
-      <Navbar>
-        <Button onClick={() => {}}>Mein Konto</Button>
-        <Button onClick={() => {}}>Merkliste</Button>
-        <Button onClick={() => {}}>Warenkorb</Button>
-      </Navbar>
-      <div className="display">
-        <h1>Art</h1>
-        <span>Shop</span>
+    <ThemeContext.Provider value={darkMode.toString()}>
+      <div className={`App ${darkMode ? "dark" : "light"}`}>
+        <div className="width-max">
+        <Navbar>
+          <div className="display">
+            <h1>Art</h1>
+            <span>Shop</span>
+          </div>
+          <div>
+            <Button onClick={() => {}}>Mein Konto</Button>
+            <Button onClick={() => {}}>Merkliste</Button>
+            <Button onClick={() => {}}>Warenkorb</Button>
+            <ReactSwitch
+              onChange={setDarkMode}
+              checked={darkMode}
+              offColor={"#f49b27"}
+              onColor={"#f49b27"}
+              checkedIcon={<CloudMoon />}
+              uncheckedIcon={<Sun />}
+            />
+          </div>
+        </Navbar>
+        <ImageSlider images={sliderImages} />
+        <h2>Kunstgalerie</h2>
+        <h3>{images.fotografie[0].type}</h3>
+        <Gallery images={images.fotografie} />
+        <h3>{images.kunst[0].type}</h3>
+        <Gallery images={images.kunst} />
+        <h3>{images.graffiti[0].type}</h3>
+        <Gallery images={images.graffiti} />
+        </div>
       </div>
-      <ImageSlider images={sliderImages} />
-      <h1>Gallery</h1>
-      <Gallery images={images.fotografie} />
-      <Gallery images={images.kunst} />
-      <Gallery images={images.graffiti} />
-    </>
+    </ThemeContext.Provider>
   );
 }
 
